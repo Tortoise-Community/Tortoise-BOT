28f16f64f3002f2c7f16d3e14fef51a410b6db03
Braindead <albertopoljak@gmail.com>
2019-10-01 20:29:11 +0200

    Added refactored cmd_error_handler cog

---
+++ cogs/cmd_error_handler.py
@@ -0,0 +1,79 @@
+import traceback
+import math
+import sys
+import discord
+from discord.ext import commands
+
+
+class CommandErrorHandler(commands.Cog):
+    """
+    Source https://gist.github.com/AileenLumina/510438b241c16a2960e9b0b014d9ed06
+
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        # If command has local error handler, return
+        if hasattr(ctx.command, "on_error"):
+            return
+
+        # Get the original exception
+        error = getattr(error, "original", error)
+
+        if isinstance(error, commands.CommandNotFound):
+            return
+
+        if isinstance(error, commands.BotMissingPermissions):
+            missing = [perm.replace("_", " ").replace("guild", "server").title() for perm in error.missing_perms]
+            if len(missing) > 2:
+                fmt = "{}, and {}".format("**, **".join(missing[:-1]), missing[-1])
+            else:
+                fmt = " and ".join(missing)
+            _message = "I need the **{}** permission(s) to run this command.".format(fmt)
+            await ctx.send(_message)
+            return
+
+        if isinstance(error, commands.DisabledCommand):
+            await ctx.send("This command has been disabled.")
+            return
+
+        if isinstance(error, commands.CommandOnCooldown):
+            await ctx.send("This command is on cooldown, please retry in {}s.".format(math.ceil(error.retry_after)))
+            return
+
+        if isinstance(error, commands.MissingPermissions):
+            missing = [perm.replace("_", " ").replace("guild", "server").title() for perm in error.missing_perms]
+            if len(missing) > 2:
+                fmt = "{}, and {}".format("**, **".join(missing[:-1]), missing[-1])
+            else:
+                fmt = " and ".join(missing)
+            _message = "You need the **{}** permission(s) to use this command.".format(fmt)
+            await ctx.send(_message)
+            return
+
+        if isinstance(error, commands.UserInputError):
+            await ctx.send(f"Invalid command input: {error}")
+            return
+
+        if isinstance(error, commands.NoPrivateMessage):
+            try:
+                await ctx.author.send("This command cannot be used in direct messages.")
+            except discord.Forbidden:
+                pass
+            return
+
+        if isinstance(error, commands.CheckFailure):
+            await ctx.send("You do not have permission to use this command.")
+            return
+
+        # Ignore all other exception types, but print them to stderr
+        print("Ignoring exception in command {}:".format(ctx.command), file=sys.stderr)
+
+        traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+                
+
+def setup(bot):
+    bot.add_cog(CommandErrorHandler(bot))
