e967e9e25ceeb3d5c06d9e676361a7ee192fd2b0
Braindead <albertopoljak@gmail.com>
2020-05-05 00:20:20 +0200

    Fixed code_submissions_channel_id

--- bot/constants.py
+++ bot/constants.py
@@ -20,7 +20,7 @@ react_for_roles_channel_id = 603651772950773761
 
 mod_mail_report_channel_id = 693790120712601610
 bug_reports_channel_id = 693790120712601610
-code_submissions_channel_id = 61007918556984115
+code_submissions_channel_id = 610079185569841153
 
 # Log Channel IDs
 system_log_channel_id = 593883395436838942
