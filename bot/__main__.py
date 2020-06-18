9dbab0c7f5348754746f5f59d68f0c8a0cfc6093
Braindead <albertopoljak@gmail.com>
2020-06-18 22:39:55 +0200

    Extensions are now loaded after first on_ready

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,8 +1,6 @@
 import os
 import logging
-import traceback
 from sys import stdout
-from pathlib import Path
 
 from dotenv import load_dotenv
 
@@ -24,31 +22,6 @@ console = logging.StreamHandler(stdout)
 console.setFormatter(formatter)
 console_logger.addHandler(console)
 
-
-# If not empty then only these will be loaded. Good for local debugging.
-allowed_extensions = ()
-banned_extensions = ("captcha_verification", "test")
-root_logger.info(f"Banned extension: {banned_extensions}")
-
 load_dotenv()
 bot = Bot(prefix="t.")
-
-
-for extension_path in Path("bot/cogs").glob("*.py"):
-    extension_name = extension_path.stem
-
-    if extension_name in banned_extensions:
-        continue
-    elif allowed_extensions and extension_name not in allowed_extensions:
-        continue
-
-    dotted_path = f"bot.cogs.{extension_name}"
-
-    try:
-        bot.load_extension(dotted_path)
-        console_logger.info(f"loaded {dotted_path}")
-    except Exception as e:
-        traceback_msg = traceback.format_exception(etype=type(e), value=e, tb=e.__traceback__)
-        console_logger.info(f"Failed to load cog {dotted_path} - traceback:{traceback_msg}")
-
 bot.run(os.getenv("BOT_TOKEN"))
