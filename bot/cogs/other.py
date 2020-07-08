b1fc873e01dae568ee04f171c3b58a53d3dadc03
Braindead <albertopoljak@gmail.com>
2020-07-08 22:33:19 +0200

    Easter stuff

--- bot/cogs/other.py
+++ bot/cogs/other.py
@@ -215,6 +215,35 @@ class Other(commands.Cog):
         """Shows the link to our paste service"""
         await ctx.send(embed=info(f":page_facing_up: {tortoise_paste_service_link}", ctx.me, title=""))
 
+    @commands.command(aliases=['this'])
+    async def zen(self, ctx):
+        zen = """
+            Beautiful is better than ugly.
+            Explicit is better than implicit.
+            Simple is better than complex.
+            Complex is better than complicated.
+            Flat is better than nested.
+            Sparse is better than dense.
+            Readability counts.
+            Special cases aren't special enough to break the rules.
+            Although practicality beats purity.
+            Errors should never pass silently.
+            Unless explicitly silenced.
+            In the face of ambiguity, refuse the temptation to guess.
+            There should be one-- and preferably only one --obvious way to do it.
+            Although that way may not be obvious at first unless you're Dutch.
+            Now is better than never.
+            Although never is often better than *right* now.
+            If the implementation is hard to explain, it's a bad idea.
+            If the implementation is easy to explain, it may be a good idea.
+            Namespaces are one honking great idea -- let's do more of those!
+        """
+        await ctx.send(embed=info(zen, ctx.me, title="The Zen of Python, by Tim Peters"))
+
+    @commands.command(aliases=['xkcd'])
+    async def antigravity(self, ctx):
+        await ctx.send(embed=info("https://xkcd.com/353/", ctx.me, title=""))
+
 
 def setup(bot):
     bot.add_cog(Other(bot))
