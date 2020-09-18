3cc544030dc7808964c686142a6dc9b06d1b9665
ryuga <vaishnav2353@gmail.com>
2020-09-19 02:04:23 +0530

    Move git command to git cog

---
+++ bot/cogs/github.py
@@ -0,0 +1,23 @@
+import aiohttp
+
+import discord
+from discord.ext import commands
+
+from bot.cogs.utils.embed_handler import info
+from bot.constants import github_repo_link
+
+
+class Github(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.session = aiohttp.ClientSession()
+
+    @commands.command(aliases=["git"])
+    async def github(self, ctx):
+        """GitHub repository"""
+        embed = info(f"[Tortoise github repository]({github_repo_link})", ctx.me, "Github")
+        await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Github(bot))
