127ad3b56f2fba6dd82de59af6b438f593620b00
Braindead <albertopoljak@gmail.com>
2020-12-08 18:54:50 +0100

    Removed fun cog Renamed other cog to misc Moved commands from fun cog to misc cog Better pfp command, now uses png which is more supported on devices than webm, and also increased avatar size to max supported.

--- bot/cogs/misc.py
+++ bot/cogs/misc.py
@@ -12,7 +12,7 @@ from bot.utils.checks import check_if_it_is_tortoise_guild
 from bot.constants import embed_space, tortoise_paste_service_link
 
 
-class Other(commands.Cog):
+class Miscellaneous(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.process = psutil.Process(os.getpid())
@@ -21,11 +21,38 @@ class Other(commands.Cog):
     @commands.command()
     @commands.has_guild_permissions(manage_messages=True)
     async def say(self, ctx, *, message):
-        """Says something.
-        To prevent abuse only moderators and above can use this."""
+        """
+        Makes the bot reply with passed string while also deleting your message.
+
+        To prevent abuse only moderators and above can use this.
+        """
         await ctx.message.delete()
         clean = await commands.clean_content().convert(ctx, message)
         await ctx.send(clean)
+
+    @commands.command()
+    async def slap(self, ctx, member: discord.Member):
+        """Slaps a member."""
+        if ctx.author == member:
+            embed = info(f"{member.mention} slapped him/her self LOL", ctx.me, "Slap!")
+            img_url = "https://media.giphy.com/media/j1zuL4htGTFQY/giphy.gif"
+        else:
+            embed = info(f"{member.mention} got slapped in the face by: {ctx.author.mention}!", ctx.me, "Slap!")
+            img_url = "https://66.media.tumblr.com/05212c10d8ccfc5ab190926912431344/tumblr_mt7zwazvyi1rqfhi2o1_400.gif"
+        embed.set_image(url=img_url)
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def shoot(self, ctx, member: discord.Member):
+        """Shoots a member."""
+        embed = info(f"{member.mention} shot by {ctx.author.mention}  :gun: :boom:", ctx.me, "Boom!")
+        embed.set_image(url="https://i.gifer.com/XdhK.gif")
+        await ctx.send(embed=embed)
+
+    @commands.command(aliases=["table", "flip"])
+    async def throw(self, ctx):
+        """Throw a table in anger."""
+        await ctx.send("```(╯°□°)╯︵ ┻━┻```")
 
     @commands.command()
     async def members(self, ctx):
@@ -44,14 +71,14 @@ class Other(commands.Cog):
     async def pfp(self, ctx, member: discord.Member = None):
         """Displays the profile picture of a member."""
         if member is None:
-            message, url = "Your avatar", ctx.author.avatar_url
+            message, member = "Your avatar", ctx.author
         elif member == ctx.me:
-            message, url = "My avatar", member.avatar_url
+            message = "My avatar"
         else:
-            message, url = f"{member} avatar", member.avatar_url
+            message = f"{member} avatar"
 
         embed = info(message, ctx.me)
-        embed.set_image(url=url)
+        embed.set_image(url=member.avatar_url_as(static_format=".png", size=4096))
         await ctx.send(embed=embed)
 
     @commands.command()
@@ -246,4 +273,4 @@ class Other(commands.Cog):
 
 
 def setup(bot):
-    bot.add_cog(Other(bot))
+    bot.add_cog(Miscellaneous(bot))
