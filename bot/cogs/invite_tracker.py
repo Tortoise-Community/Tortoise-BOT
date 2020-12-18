697e4fe7c6a3b6a7cbae1aefd4654d45276cdc06
Braindead <albertopoljak@gmail.com>
2020-12-18 16:04:45 +0100

    Refactored invite PR Mostly PEP8 stuff

--- bot/cogs/invite_tracker.py
+++ bot/cogs/invite_tracker.py
@@ -1,35 +1,35 @@
-import discord
 from discord.ext import commands
+
 from bot.utils import invite_help
 
-class invite_tracker(commands.Cog):
+
+class InviteTracker(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.tracker = invite_help.TRACKER(bot)
 
     @commands.Cog.listener()
     async def on_ready(self):
-        await self.tracker.ALL_INVITES()
+        await self.tracker.all_invites()
 
     @commands.Cog.listener()
     async def on_invite_create(self, invite):
-        await self.tracker.UPDATE_INVITE(invite)
+        await self.tracker.update_invite(invite)
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
-        await self.tracker.CREATE_GUILD_INVITES(guild)
+        await self.tracker.create_guild_invites(guild)
 
     @commands.Cog.listener()
     async def on_invite_delete(self, invite):
-        await self.tracker.DELETE_GUILD_INVITES(invite)
+        await self.tracker.delete_guild_invites(invite)
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
-        await self.tracker.REMOVE_INVITES(guild)
+        await self.tracker.remove_invites(guild)
 
-    @bot.event
-    async def on_member_join(member):
-        inviter = await self.tracker.GET_INVITER(member).id
-        '''
-        Do your stuff below here.
-        '''
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        # TODO
+        # inviter = await self.tracker.get_inviter(member).id
+        pass
--- bot/utils/invite_help.py
+++ bot/utils/invite_help.py
@@ -1,47 +1,47 @@
-import discord
-import asyncio
 import sys
 
-class CACHE(object):
-    
+import discord
+
+
+class CACHE:
     def __init__(self):
         self.cache = {}
         self.size = 40
-        
+
     def __iter__(self):
         return self.cache
 
-class TRACKER(object):
-    
+
+class TRACKER:
     def __init__(self, bot):
         self.bot = bot
         self.__cache = CACHE()
-        
-    async def ALL_INVITES(self):
+
+    async def all_invites(self):
         for guild in self.bot.guilds:
             self.__cache.cache[guild.id] = {}
             try:
-                invs = await guild.invites()
-                for invite in invs:
+                invites = await guild.invites()
+                for invite in invites:
                     if invite.inviter not in self.__cache.cache[guild.id].keys():
                         self.__cache.cache[guild.id][invite.inviter] = []
                     self.__cache.cache[guild.id][invite.inviter].append(invite)
                 self.__cache.size = sys.getsizeof(self.__cache.cache)
             except discord.errors.Forbidden:
                 pass
-        
-    async def UPDATE_INVITES(self, invite):
+
+    async def update_invite(self, invite):
         try:
-            if not invite.guild.id in self.__cache.cache.keys():
+            if invite.guild.id not in self.__cache.cache.keys():
                 self.__cache.cache[invite.guild.id] = {}
-            if not invite.inviter in self.__cache.cache[invite.guild.id].keys():
+            if invite.inviter not in self.__cache.cache[invite.guild.id].keys():
                 self.__cache.cache[invite.guild.id][invite.inviter] = []
             self.__cache.cache[invite.guild.id][invite.inviter].append(invite)
             self.__cache.size = sys.getsizeof(self.__cache.cache)
         except discord.errors.Forbidden:
             return
-    
-    async def REMOVE_INVITES(self, invite):
+
+    async def remove_invites(self, invite):
         for key in self.__cache.cache:
             for lists in self.__cache.cache[key]:
                 user = self.__cache.cache[key][lists]
@@ -49,39 +49,38 @@ class TRACKER(object):
                     self.__cache.cache[key][lists].remove(invite)
                     self.__cache.size = sys.getsizeof(self.__cache.cache)
                     break
-                    
-    async def DELETE_GUILD_INVITES(self, guild):
+
+    async def delete_guild_invites(self, guild: discord.Guild):
         if guild.id in self.__cache.cache.keys():
             self.__cache.size = sys.getsizeof(self.__cache.cache)
             del self.__cache.cache[guild.id]
-                
-    async def CREATE_GUILD_INVITES(self, guild):
+
+    async def create_guild_invites(self, guild: discord.Guild):
         try:
-            invs = await guild.invites()
+            invites = await guild.invites()
             self.__cache.cache[guild.id] = {}
-            for invite in invs:
-                if not invite.inviter in self.__cache.cache[guild.id].keys():
+            for invite in invites:
+                if invite.inviter not in self.__cache.cache[guild.id].keys():
                     self.__cache.cache[guild.id][invite.inviter] = []
                 self.__cache.cache[guild.id][invite.inviter].append(invite)
             self.__cache.size = sys.getsizeof(self.__cache.cache)
         except discord.errors.Forbidden:
             return
-        
-    async def GET_INVITER(self, member):
-        invited_by = None
-        invs = {}
+
+    async def get_inviter(self, member: discord.Member):
+        invites = {}
         try:
             new_invites = await member.guild.invites()
         except discord.errors.Forbidden:
             return
         for invite in new_invites:
-            if not invite.inviter in invs.keys():
-                invs[invite.inviter] = []
-            invs[invite.inviter].append(invite)
-        for new_invite_key in invs:
+            if invite.inviter not in invites.keys():
+                invites[invite.inviter] = []
+            invites[invite.inviter].append(invite)
+        for new_invite_key in invites:
             for cached_invite_key in self.__cache.cache[member.guild.id]:
                 if new_invite_key == cached_invite_key:
-                    new_invite_list = invs[new_invite_key]
+                    new_invite_list = invites[new_invite_key]
                     cached_invite_list = self.__cache.cache[member.guild.id][cached_invite_key]
                     for new_invite in new_invite_list:
                         for old_invite in cached_invite_list:
@@ -90,6 +89,5 @@ class TRACKER(object):
                                 cached_invite_list.append(new_invite)
                                 self.__cache.size = sys.getsizeof(self.__cache.cache)
                                 return new_invite_key
-                                break
         else:
             return None
