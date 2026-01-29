c7f583dc0870cbf40232b9fdf60990509c1a50fb
Ryuga <vaishnav2353@gmail.com>
2026-01-29 17:53:00 +0530

    Enable invite tracker cog

--- bot/bot.py
+++ bot/bot.py
@@ -21,7 +21,7 @@ console_logger = logging.getLogger("console")
 
 class Bot(commands.Bot):
     # If not empty then only these will be loaded. Good for local debugging. If empty all found are loaded.
-    allowed_extensions = ("tortoise_dm", "github", "security", "health", "piston")
+    allowed_extensions = ("tortoise_dm", "github", "security", "health", "piston", "invite_tracker")
     banned_extensions = ("advent_of_code",)
     build_version = "mystery-build"
 
--- bot/cogs/invite_tracker.py
+++ bot/cogs/invite_tracker.py
@@ -2,14 +2,21 @@ from discord import Invite, Member
 from discord.ext import commands
 
 from bot.utils import invite_help, embed_handler
-from bot.constants import tortoise_guild_id, successful_verifications_channel_id
+from bot.constants import tortoise_guild_id, system_log_channel_id
 
 
 class InviteTracker(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.tracker = invite_help.GuildInviteTracker(self.bot.get_guild(tortoise_guild_id))
-        self.log_channel = self.bot.get_channel(successful_verifications_channel_id)
+        self.guild = None
+        self.tracker = None
+        self.log_channel = None
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.guild = self.bot.get_guild(tortoise_guild_id)
+        self.tracker = invite_help.GuildInviteTracker(self.guild)
+        self.log_channel = self.bot.get_channel(system_log_channel_id)
         self.bot.loop.create_task(self.tracker.refresh_invite_cache())
 
     @commands.Cog.listener()
@@ -22,19 +29,55 @@ class InviteTracker(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member: Member):
+        if member.guild.id != tortoise_guild_id:
+            return
         inviter = await self.tracker.track_invite()
+        created_at = f"<t:{int(member.created_at.timestamp())}:R>"
+
         if inviter:
-            await self.log_channel.send(embed=embed_handler.info(
-                f"New member {member} was invited by {inviter}",
-                member=member,
-                title=""
-            ))
+            msg = (
+                f"**Member:** {member} (`{member.id}`)\n"
+                f"**Invited by:** {inviter}\n"
+                f"**Account created:** {created_at}"
+            )
         else:
-            await self.log_channel.send(embed=embed_handler.info(
-                f"New member {member} joined through discord Discovery",
+            msg = (
+                f"**Member:** {member} (`{member.id}`)\n"
+                f"**Invited by:** Discord Discovery / Vanity\n"
+                f"**Account created:** {created_at}"
+            )
+
+        await self.log_channel.send(
+            embed=embed_handler.info(
+                msg,
                 member=member,
-                title=""
-            ))
+                title="Member Joined"
+            )
+        )
+
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member: Member):
+        if member.guild.id != tortoise_guild_id:
+            return
+
+        joined_at = (
+            f"<t:{int(member.joined_at.timestamp())}:R>"
+            if member.joined_at else "Unknown"
+        )
+
+        msg = (
+            f"**Member:** {member} (`{member.id}`)\n"
+            f"**Joined at:** {joined_at}"
+        )
+
+        await self.log_channel.send(
+            embed=embed_handler.info(
+                msg,
+                member=member,
+                title="Member Left"
+            )
+        )
 
 
 async def setup(bot):
